5d7e40cf583dfe68b0d2c94b94985022c8f22679
xxleng <272361997@qq.com>
2020-11-26 02:42:18 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
+// use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -22,13 +23,20 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+
+
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
 
-        if(! $topic->slug){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-        }
 
+
+    }
+
+    public function saved(Topic $topic)
+    {
+        if(! $topic->slug){
+            dispatch(new TranslateSlug($topic));
+        }
     }
 }
